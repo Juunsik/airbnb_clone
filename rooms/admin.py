212f16bd7a01ed30c4f4f623d50f5e58fdb70e9e
Heo-JunHyeok <junhyeok_1996@naver.com>
2022-03-10 03:04:20 +0900

    room admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -16,7 +16,74 @@ class RoomAdmin(admin.ModelAdmin):
 
     """Room Admin Definition"""
 
-    pass
+    # Rooms 정보 카테고리로 구성 셋
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book")},
+        ),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),  # 접을 수 있는 섹션으로 만들 수 있다.
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    # 원하는 항목만 리스트로 화면 출력
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    # 정렬
+    ordering = ("name", "price", "bedrooms")
+
+    # 오른쪽 카테고리 필터
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    # 검색 설정
+    search_fields = (
+        "city",
+        "^host__username",
+    )
+
+    # many_to_many 항목 편집을 편하게
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    def count_amenities(self, obj):  # self: admin class(RoomAdmin), obj: 현재 row
+        print(obj.amenities.all())  # console에 현재 row 출력
+        return "Potato"
 
 
 @admin.register(models.Photo)
