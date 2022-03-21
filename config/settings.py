0368c87cc43545377e42ddcf4f4b2cf906c80a69
Heo-JunHyeok <junhyeok_1996@naver.com>
2022-03-21 19:03:24 +0900

    photo admin

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 AUTH_USER_MODEL = "users.User"
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
+
+# /media/: 절대경로, media/: 상대경로
+MEDIA_URL = "/media/"
