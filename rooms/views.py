1d933fecb7f6466e4165b3c912236b4cf4bbb9ec
Heo-JunHyeok <junhyeok_1996@naver.com>
2022-04-14 05:56:22 +0900

    paginator, CBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,59 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    all_rooms = models.Room.objects.all()
-    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+# Class Based View 사용(3번째 방법)
+# core.urls.py urlpatterns path : HomeView.as_view()
+# templates/rooms/room_list.html 파일 필요
+class HomeView(ListView):
+
+    """HomeView Definition"""
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
+
+
+# django paginator 사용(2번째 방법)
+# core.urls.py urlpatterns path 일치해야 함
+# templates/rooms/home.html 파일 필요
+# def all_rooms(request):
+#     page = request.GET.get("page", 1)
+#     room_list = models.Room.objects.all()
+#     paginator = Paginator(
+#         room_list, 10, orphans=5
+#     )  # orphans: 나머지, orphans보다 적게 남은 object를 마지막 이전 페이지에 추가로 출력
+
+#     try:
+#         rooms = paginator.page(int(page))
+#         # rooms = paginator.get_page(page),get_page는 try~except 필요없음, paginator.page는 get_page보다 좀 더 에러 컨트롤 할 수 있다.
+#         return render(request, "rooms/home.html", {"page": rooms})
+#     except EmptyPage:
+#         return redirect("/")
+
+# 수동으로 paginator 만들기(1번째 방법,all_rooms func안에 위치)
+# page = int(page or 1)  # page= 일 경우 default
+# page_size = 10
+# limit = page_size * page
+# offset = limit - page_size
+# all_rooms = models.Room.objects.all()[offset:limit]
+# page_count = ceil(models.Room.objects.count() / page_size)
+# return render(
+#     request,
+#     "rooms/home.html",
+#     context={
+#         "rooms": all_rooms,
+#         "page": page,
+#         "page_count": page_count,
+#         "page_range": range(1, page_count),
+#     },
+# )
