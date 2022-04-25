dd8d183b46fa18bc89407842239f783539adadb5
Heo-JunHyeok <junhyeok_1996@naver.com>
2022-04-26 00:17:08 +0900

    login FormView

--- users/views.py
+++ users/views.py
@@ -1,12 +1,15 @@
 from django.views import View
+from django.views.generic import FormView
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
+# Use View
 class LoginView(View):
     def get(self, request):
-        form = forms.LoginForm(initial={"email": "its@las.com"})
+        form = forms.LoginForm(initial={"email": "wnsgur3202@naver.com"})
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
@@ -25,6 +28,32 @@ def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
 
+
+# ========================================================================================
+
+# Use FormView
+# class LoginView(FormView):
+
+#     template_name = "users/login.html"
+#     form_class = forms.LoginForm
+#     success_url = reverse_lazy("core:home")
+#     initial = {"email": "wnsgur3202@naver.com"}
+
+#     def form_valid(self, form):
+#         email = form.cleaned_data.get("email")
+#         password = form.cleaned_data.get("password")
+#         user = authenticate(self.request, username=email, password=password)
+#         if user is not None:
+#             login(self.request, user)
+#         return super().form_valid(form)
+
+
+# def log_out(request):
+#     logout(request)
+#     return redirect(reverse("core:home"))
+
+
+# =========================================================================================
 
 # DBV base
 # class LoginView(View):
