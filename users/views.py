af584743b7f39fc26e779bf2650823e9491f4cb0
Heo-JunHyeok <junhyeok_1996@naver.com>
2022-04-28 02:06:25 +0900

    SignUp with or without ModelForm

--- users/views.py
+++ users/views.py
@@ -7,50 +7,50 @@ from . import forms
 
 
 # Use View
-class LoginView(View):
-    def get(self, request):
-        form = forms.LoginForm(initial={"email": "wnsgur3202@naver.com"})
-        return render(request, "users/login.html", {"form": form})
+# class LoginView(View):
+#     def get(self, request):
+#         form = forms.LoginForm(initial={"email": "wnsgur3202@naver.com"})
+#         return render(request, "users/login.html", {"form": form})
 
-    def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get("email")
-            password = form.cleaned_data.get("password")
-            user = authenticate(request, username=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect(reverse("core:home"))
-        return render(request, "users/login.html", {"form": form})
-
-
-def log_out(request):
-    logout(request)
-    return redirect(reverse("core:home"))
-
-
-# ========================================================================================
-
-# Use FormView
-# class LoginView(FormView):
-
-#     template_name = "users/login.html"
-#     form_class = forms.LoginForm
-#     success_url = reverse_lazy("core:home")
-#     initial = {"email": "wnsgur3202@naver.com"}
-
-#     def form_valid(self, form):
-#         email = form.cleaned_data.get("email")
-#         password = form.cleaned_data.get("password")
-#         user = authenticate(self.request, username=email, password=password)
-#         if user is not None:
-#             login(self.request, user)
-#         return super().form_valid(form)
+#     def post(self, request):
+#         form = forms.LoginForm(request.POST)
+#         if form.is_valid():
+#             email = form.cleaned_data.get("email")
+#             password = form.cleaned_data.get("password")
+#             user = authenticate(request, username=email, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect(reverse("core:home"))
+#         return render(request, "users/login.html", {"form": form})
 
 
 # def log_out(request):
 #     logout(request)
 #     return redirect(reverse("core:home"))
+
+
+# ========================================================================================
+
+# Use FormView
+class LoginView(FormView):
+
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+    initial = {"email": "wnsgur3202@naver.com"}
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
 
 
 # =========================================================================================
@@ -69,3 +69,26 @@ def log_out(request):
 #         pass
 #     elif request.method =="POST":
 #         pass
+
+
+# ===========================================================================================================
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "first_name": "wns",
+        "last_name": "gur",
+        "email": "test@naver.com",
+    }
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
