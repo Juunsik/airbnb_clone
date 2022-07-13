33b9db3122d232de2f806fc56252d3c830b1f10d
Heo-JunHyeok <junhyeok_1996@naver.com>
2022-07-13 23:46:40 +0900

    login, sign up

--- users/views.py
+++ users/views.py
@@ -81,11 +81,6 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
-    initial = {
-        "first_name": "wns",
-        "last_name": "gur",
-        "email": "test@naver.com",
-    }
 
     def form_valid(self, form):
         form.save()
