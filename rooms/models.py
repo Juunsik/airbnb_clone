dcae852d58983e63e6c67fa2503f556dd8011f09
Heo-JunHyeok <junhyeok_1996@naver.com>
2022-08-24 19:49:01 +0900

    Styling the Room part One

--- rooms/models.py
+++ rooms/models.py
@@ -113,3 +113,9 @@ class Room(core_models.TimeStapedModel):
     def get_next_four_photos(self):
         photos = self.photos.all()[1:5]
         return photos
+
+    def get_beds(self):
+        if self.beds == 1:
+            return "1 bed"
+        else:
+            return f"{self.beds} beds"
