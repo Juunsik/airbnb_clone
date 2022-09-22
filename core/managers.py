9e961769055db3e8d8cbe2af415b82a3dfe4c33c
Heo-JunHyeok <junhyeok_1996@naver.com>
2022-09-22 15:57:25 +0900

    #25.8 Conversation Styles

--- core/managers.py
+++ core/managers.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import UserManager
 
 
 class CustomModelManager(models.Manager):
-    def get_or_none(self, *args, **kwargs):
+    def get_or_none(self, **kwargs):
         try:
-            return self.get(*args, **kwargs)
+            return self.get(**kwargs)
         except self.model.DoesNotExist:
             return None
+
+
+class CustomUserManager(CustomModelManager, UserManager):
+    pass
